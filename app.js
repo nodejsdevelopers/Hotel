2e68a92fc27d34707aa3a588c1d387aae863fecb
nodejsdevelopers <46884154+nodejsdevelopers@users.noreply.github.com>
2020-03-16 07:50:33 +0530

    new changes
    new changes

--- app.js
+++ app.js
@@ -1,61 +1,3 @@
-/* var mysql = require('mysql');
-var express = require('express')
-var app = express()
-
-
-var session = require('express-session');
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
-
-
-/*
-var sess = req.session;  //initialize session variable
-req.session.userId = results[0].id; //set user id
-req.session.user = results[0];//set user name
-
-
-*/
-/* 
- req.session.destroy(function(err) {
-      //cal back method
-   })
-
-var con = mysql.createConnection({
-    user: "root",
-    password: "Password@123",
-    database: "hotel_db",
-    host: "127.0.0.1",
-    dialect: "mysql"
-});
-
-/*
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "SHOW DATABASES;";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result);
-  });
-
-  
-});
-
-app.get('/loginCustomer', function (req, res) {
-  res.send('hello world')
-})
-
-app.listen(3000)
-
-
- */ 
-///////////////////////////////////////////////////////////
-
 
 var express = require('express')
   , user = require('./routes/user')
@@ -91,3 +33,4 @@ app.use("/booking", booking);
  
 //Middleware
 app.listen(8080)
+
